8a937d16e28424babba5d96d6faed7b2579f69ff
sriramkora <sriramkora5000@gmail.com>
2023-11-30 23:09:40 -0500

    updated login page styling

--- moodlist/src/Pages/AuthenticationPage.jsx
+++ moodlist/src/Pages/AuthenticationPage.jsx
@@ -1,31 +1,50 @@
 import { useState, useEffect } from "react";
 import { Tabs, Tab, Button, Form } from "react-bootstrap";
-import { json, redirect } from 'react-router-dom';
-import AuthForm from '../Components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../Components/AuthForm";
+import classes from "../CSS/AunthenticationPage.module.css";
+
 function AuthenticationPage() {
-  return <AuthForm />;
+  return (
+    <>
+      <div className={classes.logo}>
+        <h1>Mood.List</h1>
+      </div>
+      <div className={classes.loginPage}>
+        <div className={classes.loginFormContainer}>
+          <div className={classes.loginForm}>
+            <AuthForm />
+          </div>
+        </div>
+      </div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png"
+        alt="spotifyLogo"
+      />
+    </>
+  );
 }
 
 export default AuthenticationPage;
 
 export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
-  const mode = searchParams.get('mode') || 'login';
+  const mode = searchParams.get("mode") || "login";
 
-  if (mode !== 'login' && mode !== 'signup') {
-    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  if (mode !== "login") {
+    throw json({ message: "Unsupported mode." }, { status: 422 });
   }
 
   const data = await request.formData();
   const authData = {
-    email: data.get('email'),
-    password: data.get('password'),
+    email: data.get("email"),
+    password: data.get("password"),
   };
 
-  const response = await fetch('http://localhost:8080/' + mode, {
-    method: 'POST',
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(authData),
   });
@@ -35,18 +54,16 @@ export async function action({ request }) {
   }
 
   if (!response.ok) {
-    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+    throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
 
   const resData = await response.json();
   const token = resData.token;
 
-  localStorage.setItem('token', token);
+  localStorage.setItem("token", token);
   const expiration = new Date();
   expiration.setHours(expiration.getHours() + 1);
-  localStorage.setItem('expiration', expiration.toISOString());
+  localStorage.setItem("expiration", expiration.toISOString());
 
-  return redirect('/');
+  return redirect("/");
 }
-
- 
