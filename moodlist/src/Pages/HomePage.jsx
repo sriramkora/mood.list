d79d4e0565afd2c74805f525aff3218edfa1ad5e
erinvmiller <erin.miller@wustl.edu>
2023-12-01 18:59:12 -0500

    Home page content and styling

--- moodlist/src/Pages/HomePage.jsx
+++ moodlist/src/Pages/HomePage.jsx
@@ -1,6 +1,24 @@
-export default function HomePage() {
+import React from "react";
+import classes from "../CSS/HomePage.module.css";
 
-    return(
-        <h1>HomePage</h1>
-    )
+export default function HomePage() {
+    return (
+        <div className={classes.homePage}>
+        <div className={classes.overlapWrapper}>
+          <div className={classes.overlap}>
+            <div className={classes.group}>
+              <div className={classes.overlapGroup}>
+                <div className={classes.textWrapper}>Generate</div>
+              </div>
+            </div>
+            <div className={classes.overlapGroupWrapper}>
+              <textarea className={classes.input} placeholder="Please enter some text, such as a poem, song lyrics, or an essay. Max 500 words." />
+            </div>
+            <div className={classes.textWrapper2}>mood.list</div>
+            <div className={classes.group2}>
+            </div>
+          </div>
+        </div>
+      </div>
+      );
 }
