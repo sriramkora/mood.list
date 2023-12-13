02a1bc4d9d36226a401cace4c30b9cd6fc0404c0
sriramkora <sriramkora5000@gmail.com>
2023-12-12 20:00:07 -0500

    formatted code for home result and account page
    formatted code for home result and account page and did a bug fix in homepage

--- moodlist/src/Pages/HomePage.jsx
+++ moodlist/src/Pages/HomePage.jsx
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "../CSS/HomePage.module.css";
 import { Navigate, redirect, useSearchParams } from "react-router-dom";
 import { getAuthToken } from "../util/auth";
 import { action as logoutAction } from "./Logout";
 
-
 export default function HomePage() {
   const [searchParams] = useSearchParams();
-  const token = searchParams.get("access_token");
   const refresh_token = searchParams.get("refresh_token");
   const expires_in = searchParams.get("expires_in");
   const scope = searchParams.get("scope");
   const userid = searchParams.get("id");
   const email = searchParams.get("email");
-  
+  const token = searchParams.get("access_token");
+  const [isTokenSet, setIsTokenSet] = useState(false);
+
+  useEffect(() => {
+    if (!getAuthToken() && token) {
+      localStorage.setItem("token", token);
+      const expiration = new Date();
+      expiration.setHours(expiration.getHours() + 1);
+      localStorage.setItem("expiration", expiration.toISOString());
+      localStorage.setItem("refresh_token", refresh_token);
+      localStorage.setItem("scope", scope);
+      localStorage.setItem("userid", userid);
+      localStorage.setItem("email", email);
+
+      console.log("token set!");
+
+      window.location.reload();
+    }
+  }, [token]);
+
   if (!getAuthToken() && !token) {
     console.log("deleting token!");
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     return <Navigate to="/auth" />;
   }
-  if (!getAuthToken() && token) {
-    localStorage.setItem("token", token);
-    const expiration = new Date();
-    expiration.setHours(expiration.getHours() + 1);
-    localStorage.setItem("expiration", expiration.toISOString());
-    localStorage.setItem("refresh_token", refresh_token);
-    localStorage.setItem("scope", scope);
-    localStorage.setItem("userid", userid);
-    localStorage.setItem("email", email);
 
-    console.log("token set!")
-  }
   return (
     <div className={classes.homePage}>
       <div className={classes.overlapWrapper}>
@@ -47,7 +53,6 @@ export default function HomePage() {
             />
           </div>
           <div className={classes.titleWrapper}>mood.list</div>
-          <div className={classes.textWrapper2}>Log out</div>
         </div>
       </div>
     </div>
--- moodlist/src/Pages/ResultPage.jsx
+++ moodlist/src/Pages/ResultPage.jsx
@@ -10,45 +10,63 @@ export default function ResultPage() {
       date: "Wed, Nov 1",
       weather: "Rainy",
       description: "Lorem ipsum...",
-      embedLink: "https://open.spotify.com/embed/playlist/37nCSouvwoPLsM91nawryP?utm_source=generator",
+      embedLink:
+        "https://open.spotify.com/embed/playlist/37nCSouvwoPLsM91nawryP?utm_source=generator",
     },
     {
       date: "Wed, Nov 1",
       weather: "Rainy",
       description: "Lorem ipsum...",
-      embedLink: "https://open.spotify.com/embed/playlist/37nCSouvwoPLsM91nawryP?utm_source=generator",
+      embedLink:
+        "https://open.spotify.com/embed/playlist/37nCSouvwoPLsM91nawryP?utm_source=generator",
     },
-  ];        
+  ];
   const isClicked = false;
   return (
-  <div className={classes.accountPage}>
-    <div className={classes.div}>
-      <div className={classes.overlap}>
-        <img className={classes.rectangle} alt="Rectangle" src="rectangle-1.svg" />
-        <div className={classes.headerRect} />
-        <div className={classes.textWrapper}>mood.list</div>
-        <div className={classes.group}>
-          <div className={classes.cartButton}>
-            <div className={classes.logoutButton}>Profile</div>
-          </div>
+    <div className={classes.accountPage}>
+      <div className={classes.div}>
+        <div className={classes.overlap}>
+          <img
+            className={classes.rectangle}
+            alt="Rectangle"
+            src="rectangle-1.svg"
+          />
+          <div className={classes.headerRect} />
+          <div className={classes.textWrapper}>mood.list</div>
+          {/* <div className={classes.group}>
+            <div className={classes.cartButton}>
+              <div className={classes.logoutButton}>Profile</div>
+            </div>
+          </div> */}
+        </div>
+        <div className={classes.textWrapper6}>Results</div>
+        <div className={classes.playlistsWrapper}>
+          {playlists.map((playlist, index) => (
+            <div className={classes.playlistItem} key={index}>
+              <p className={classes.bigNumber}>{index + 1}.</p>
+              <iframe
+                className={classes.playlistPlayer}
+                src={playlist.embedLink}
+                width="50%"
+                height="152"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              ></iframe>
+              {isClicked ? (
+                <img
+                  src={checkImage}
+                  alt="check"
+                  className={classes.addButton}
+                />
+              ) : (
+                <img src={plusImage} alt="plus" className={classes.addButton} />
+              )}
+            </div>
+          ))}
         </div>
       </div>
-      <div className={classes.textWrapper6}>Results</div>
-      <div className={classes.playlistsWrapper}>
-        {playlists.map((playlist, index) => (
-          <div className={classes.playlistItem} key={index}>
-            <p className={classes.bigNumber}>{index+1}.</p>
-            <iframe className={classes.playlistPlayer} src={playlist.embedLink} width="50%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            {isClicked ? (
-              <img src={checkImage} alt="check" className={classes.addButton} />
-            ) : (
-              <img src={plusImage} alt="plus" className={classes.addButton} />
-            )}
-          </div>
-        ))}
-      </div>
     </div>
-  </div>
-  )
-
+  );
 }
