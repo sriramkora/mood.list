9176f2dc8ae93ab2b7385dd968a5dc505789d573
Allen <allenabraham505@gmail.com>
2023-12-14 11:11:34 -0500

    Remove AWS keys and secrets

--- moodlist/backend/constants/consts.js
+++ moodlist/backend/constants/consts.js
@@ -6,8 +6,8 @@ const spotifyClientsecret = process.env.SPOTIFY_CLIENT_SECRET;
 const spotifyAccountsHost = "https://accounts.spotify.com";
 const spotifyApisHost = "https://api.spotify.com";
 const spotifyAuthzScope = 'user-read-private user-read-email';
-const accessKey = 'AKIARWHYP3TLN2VVCH7U';
-const accessSecret = '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';
+const accessKey =  process.env.AWS_ACCESS_KEY;
+const accessSecret = process.env.AWS_SECRET_KEY;
 
 module.exports = {
     APP_PORT,
