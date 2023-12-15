aa8beced57d41b803a3a6c74d750b57477edaa6f
sriramkora <sriramkora5000@gmail.com>
2023-12-15 11:22:26 -0500

    Update HomePage.jsx

--- moodlist/src/Pages/HomePage.jsx
+++ moodlist/src/Pages/HomePage.jsx
@@ -111,6 +111,7 @@ export default function HomePage() {
               "getKeywords API request failed with status:",
               response.status
             );
+            retryCount++;
           }
         }
       } else {
